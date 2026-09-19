68eb32b7227fdfc48338c54bf663f456b7e574c7
aamggd <63270828+aamggd@users.noreply.github.com>
2026-09-19 08:42:33 +0300

    CI: add ANDAK customer source settings.gradle.kts

---
+++ andak_ci_src/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AndakCustomerHotfixBuild"
+include(":core:ui", ":apps:customer")
